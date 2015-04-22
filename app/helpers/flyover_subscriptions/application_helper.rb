26332a484e99c83720e7dae8915acebabf9a43fb
David Van Der Beek <earlynovrock@gmail.com>
2015-04-22 16:19:22 -0500

    use helper for charge dates

--- app/helpers/flyover_subscriptions/application_helper.rb
+++ app/helpers/flyover_subscriptions/application_helper.rb
@@ -1,5 +1,9 @@
 module FlyoverSubscriptions
   module ApplicationHelper
+    def date_from_stripe_timestamp(timestamp)
+      Time.at(timestamp).strftime("%m/%d/%Y")
+    end
+
     def method_missing method, *args, &block
       if method.to_s.end_with?('_path') or method.to_s.end_with?('_url')
         if main_app.respond_to?(method)
