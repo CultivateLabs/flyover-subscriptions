637b70ccb7c5b3b406870b4184e476420a30c09f
David Van Der Beek <earlynovrock@gmail.com>
2015-04-22 15:13:23 -0500

    update variable names in stripe stub helper

--- spec/models/flyover_subscriptions/subscription_spec.rb
+++ spec/models/flyover_subscriptions/subscription_spec.rb
@@ -11,7 +11,7 @@ module FlyoverSubscriptions
     it "updates a subscription's plan" do
       subscription = create(:subscription)
       new_plan = create(:plan)
-      expect(@customer).to receive(:update_subscription).with(plan: new_plan.stripe_id, prorate: true).and_return(true)
+      expect(stripe_customer).to receive(:update_subscription).with(plan: new_plan.stripe_id, prorate: true).and_return(true)
       subscription.plan = new_plan
       subscription.save
       expect(subscription.plan).to eq new_plan
@@ -19,17 +19,26 @@ module FlyoverSubscriptions
 
     it "sets the quantity to zero in Stripe" do
       subscription = create(:subscription)
-      expect(@subscription).to receive(:quantity=).with(0)
+      expect(stripe_subscription).to receive(:quantity=).with(0)
       subscription.set_quantity_to_zero
       expect(subscription.archived).to be_truthy
     end
 
     it "resubscribes a customer by setting quantity to 1 when an archived subscription is updated" do
       subscription = create(:subscription, archived: true)
-      expect(@subscription).to receive(:quantity=).with(1)
+      expect(stripe_subscription).to receive(:quantity=).with(1)
       subscription.updated_at = Time.now
       subscription.save
       expect(subscription.archived).to be_falsy
+    end
+
+    it "cancels the stripe subscription when the subscriber is deleted" do
+      subscription = create(:subscription)
+      
+      expect(stripe_customer).to receive(:cancel_subscription)
+      expect{
+        subscription.subscriber.destroy
+      }.to change(FlyoverSubscriptions::Subscription, :count).by(-1)
     end
   end
 end
--- spec/support/stripe.rb
+++ spec/support/stripe.rb
@@ -1,13 +1,13 @@
 module StripeHelper
-  def customer 
-    @customer ||= begin
-      customer = double(id: "token_123")
-      allow(customer).to receive(:sources).and_return(sources)
-      allow(customer).to receive(:default_source)
-      allow(customer).to receive(:card=).and_return(true)
-      allow(customer).to receive(:charges).and_return(double(data: []))
-      allow(customer).to receive(:subscriptions).and_return(subscriptions)
-      customer
+  def stripe_customer 
+    @stripe_customer ||= begin
+      stripe_customer = double(id: "token_123")
+      allow(stripe_customer).to receive(:sources).and_return(sources)
+      allow(stripe_customer).to receive(:default_source)
+      allow(stripe_customer).to receive(:card=).and_return(true)
+      allow(stripe_customer).to receive(:charges).and_return(double(data: []))
+      allow(stripe_customer).to receive(:subscriptions).and_return(subscriptions)
+      stripe_customer
     end
   end
 
@@ -28,23 +28,23 @@ module StripeHelper
   def subscriptions
     @subscriptions ||= begin
       subscriptions = double
-      allow(subscriptions).to receive(:first).and_return(subscription)
+      allow(subscriptions).to receive(:first).and_return(stripe_subscription)
       subscriptions      
     end
   end
 
-  def subscription
-    @subscription ||= begin 
-      subscription = double(quantity: 0)
-      allow(subscription).to receive(:quantity=)
-      allow(subscription).to receive(:save).and_return(true)
-      subscription
+  def stripe_subscription
+    @stripe_subscription ||= begin 
+      stripe_subscription = double(quantity: 0)
+      allow(stripe_subscription).to receive(:quantity=)
+      allow(stripe_subscription).to receive(:save).and_return(true)
+      stripe_subscription
     end
   end
 
   def stub_stripe
-    allow(Stripe::Customer).to receive(:create).and_return(customer)
-    allow(Stripe::Customer).to receive(:retrieve).and_return(customer)
+    allow(Stripe::Customer).to receive(:create).and_return(stripe_customer)
+    allow(Stripe::Customer).to receive(:retrieve).and_return(stripe_customer)
 
     allow(Stripe::Invoice).to receive(:upcoming).and_return(double(total: 100, next_payment_attempt: 7.days.from_now))
   end
