e4689e062432e5b93b896ff3b4af01c55a22b198
David Van Der Beek <earlynovrock@gmail.com>
2015-05-05 13:57:55 -0500

    Fix I18n for email subject

--- spec/mailers/flyover_subscriptions/stripe_events_mailer_spec.rb
+++ spec/mailers/flyover_subscriptions/stripe_events_mailer_spec.rb
@@ -6,7 +6,7 @@ module FlyoverSubscriptions
       let(:mail) { StripeEventsMailer.failed_charge_notify_customer(double(email: "to@example.org")) }
 
       it "renders the headers" do
-        expect(mail.subject).to eq("Failed charge notify customer")
+        expect(mail.subject).to eq I18n.t('flyover_subscriptions.stripe_events_mailer.failed_charge_notify_customer.subject')
         expect(mail.to).to eq(["to@example.org"])
         expect(mail.from).to eq(["noreply@example.com"])
       end
--- spec/mailers/previews/flyover_subscriptions/stripe_events_preview.rb
+++ spec/mailers/previews/flyover_subscriptions/stripe_events_preview.rb
@@ -1,10 +1,10 @@
 module FlyoverSubscriptions
   # Preview all emails at http://localhost:3000/rails/mailers/flyover_subscriptions/stripe_events
-  class StripeEventsPreview < ActionMailer::Preview
+  class StripeEventsMailerPreview < ActionMailer::Preview
 
     # Preview this email at http://localhost:3000/rails/mailers/flyover_subscriptions/stripe_events/failed_charge_notify_customer
     def failed_charge_notify_customer
-      StripeEvents.failed_charge_notify_customer
+      StripeEventsMailer.failed_charge_notify_customer
     end
 
   end
